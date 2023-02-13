113a2216f962ed348a8d6a065bf404a9b3d8431c
Game <popyoy7@gmail.com>
2023-02-13 10:38:18 +0700

    not done

--- flutter_front_end/lib/main.dart
+++ flutter_front_end/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_front_end/screen/student_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
+  ensureI
+
   runApp(const MyApp());
 }
 
